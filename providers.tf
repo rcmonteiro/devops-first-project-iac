daa0e36a4d6820e881982533eb2e2a6260c34bb3
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-05-09 09:31:08 -0300

    chore: migrando o estado do terraform para o s3

--- providers.tf
+++ providers.tf
@@ -5,8 +5,37 @@ terraform {
       version = "5.48.0"
     }
   }
+  backend "s3" {
+    bucket  = "devops-rocketseat-rcmonteiro-state-bucket-tf"
+    region  = "us-east-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+    profile = "rcmonteiro-iac"
+  }
 }
 
 provider "aws" {
   profile = "rcmonteiro-iac"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_state
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IaC = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  depends_on = [aws_s3_bucket.terraform_state]
 }
