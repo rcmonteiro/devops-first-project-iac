a63c46e3f59cb0bc58c2bb8768b5022a986102d9
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-05-09 10:01:00 -0300

    chore: trabalando com tfvars

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "bucket_state" {
   type        = string
-  default     = "devops-rocketseat-rcmonteiro-state-bucket-tf"
+  #default     = "devops-rocketseat-rcmonteiro-state-bucket-tf"
   description = "Bucket do estado do Terraform"
 }
